d594686cf22d74f1b5b37347e9a6ae7c47524899
lavacastred <69476013+lavacastred@users.noreply.github.com>
2020-08-19 12:33:21 -0400

    Update and rename Executer to Executer.lua

---
+++ Executer.lua
@@ -0,0 +1,26 @@
+if _G.secret == "38BFC9C5636CBC83BACC3E2DEEB7C" then
+	print("Found Secret")
+	secretkey434124124124124 = true
+else
+	print("Didnt Find Secret")
+end
+
+if secretkey434124124124124 == true then
+local gid = game.GameId
+
+
+if gid == 0 then
+	print("This Is A Blank Game :). Are you trying to steal my script?")
+	print("Very Funny Man Arent You")
+	print("Get Out Of Your Chair And Get A Life Lmao")
+	elseif gid == 192800 then
+		print("Work At A Pizza Place")
+		loadstring(game:HttpGet("https://pastebin.com/raw/cEwtwKZR",true))()
+	elseif gid == 4388574342 then
+		print("Nova Is Gay")
+		
+end
+
+
+
+end
